707a4e589e570fa7a50d88e1e18dc78dad2b1a3b
genoth <nothgwynne@gmail.com>
2017-09-17 14:03:35 -0400

    Finish new and create routes and views, plus show and index routes and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,41 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
+    @article = Article.new
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
-
-    @article.save
-    redirect_to @article
-    # render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-  # ******BELOW, refactored the above code so that the article params method can be reused in other contexts******
+  def show
+    @article = Article.find(params[:id])
+  end
 
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
+  # ******BELOW, the original code for #create route before refactoring to have a private usuable article_params method
   # def create
-  #   @article = Article.new(article_params)
+  #   @article = Article.new(params.require(:article).permit(:title, :text))
+
   #   @article.save
   #   redirect_to @article
+  #   # render plain: params[:article].inspect
   # end
-
-  # private
-  #   def article_params
-  #     params.require(:article).permit(:title, :text)
-  #   end
 end
