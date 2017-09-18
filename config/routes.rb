1f39b07bd56de151a64ebb2b80c48f0975384ad4
genoth <nothgwynne@gmail.com>
2017-09-17 23:08:00 -0500

    Implement user registration, debug

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
 
 
   root 'welcome#index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
   # this tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
